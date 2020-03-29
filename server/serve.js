06bed9c5c85a27f43601901a1304e78cfec33bfc
max bittker <maxbittker@gmail.com>
2020-03-29 02:59:03 -0700

    format

--- server/serve.js
+++ server/serve.js
@@ -1,35 +1,34 @@
-const express = require('express')
+const express = require("express");
 const fs = require("fs");
-const path = require('path');
-const bodyParser = require('body-parser');
-var randomstring = require('randomstring');
+const path = require("path");
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
 
-var crypto = require('crypto')
+const filePath = path.join(__dirname, "sketches");
+const port = 3002;
 
-const filePath = path.join(__dirname, 'sketches');
-const port = 3002
-
-
-const app = express()
+const app = express();
 
 app.use(bodyParser.text());
-app.use('/static', express.static(filePath))
+app.use("/static", express.static(filePath));
 
-app.get('/', (req, res) => {
- fs.promises.readdir(filePath).then(files=>{
-	res.send(JSON.stringify(files)) ;
-})
+let cacheFiles = null;
+app.get("/", (req, res) => {
+  fs.promises.readdir(filePath).then(files => {
+    cacheFiles = files;
+    res.send(JSON.stringify(files));
+  });
 });
 
+app.post("/upload", (req, res) => {
+  let id = crypto
+    .createHash("md5")
+    .update(req.body.toString())
+    .digest("hex")
+    .slice(0, 5);
 
-app.post('/upload', (req, res) => {
-	console.log(JSON.stringify(req.body));
-	let id = crypto.createHash('md5').update(req.body.toString()).digest("hex").slice(0,5);
-	//randomstring.generate({length: 5, readable:true, capitalization:'lowercase'});
-	fs.promises.writeFile(path.join(filePath,id),req.body.toString());
-	res.send(JSON.stringify({id})) ;
-
+  fs.promises.writeFile(path.join(filePath, id), req.body.toString());
+  res.send(JSON.stringify({ id }));
 });
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
