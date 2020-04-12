e30276544f9a94586ac9f4526fd36b649fd2de80
max bittker <maxbittker@gmail.com>
2020-04-12 14:52:46 -0700

    build

--- docs/demos/cute.glsl
+++ docs/demos/cute.glsl
@@ -9,8 +9,8 @@ void main() {
   blur += getCam(uv + vec2(0., -offset.y)) / 4.;
 
   float freckles = clamp(noise(uv * 100.) - 0.6, 0., 1.0);
-  float frecklePattern = min(length(uv - faceCenter + vec2(0.2, 0.)),
-                             length(uv - faceCenter - vec2(0.2, 0.)));
+  float frecklePattern = min(length(uv - leftEye + vec2(0.0, 0.2)),
+                             length(uv - rightEye + vec2(0.0, 0.2)));
   frecklePattern = .15 - frecklePattern;
   frecklePattern = clamp(frecklePattern, 0., 0.5);
   freckles *= frecklePattern;
@@ -26,8 +26,8 @@ void main() {
   // lashes and pupils darker
   if (getEye(uv) > 0.3) {
     color = getCam(uv);
-    if (luma(color) < 0.3) {
-      color /= 2.;
+    if (luma(color) < 0.2) {
+      color /= 1.5;
     }
   }
   // eyes brighter
@@ -39,9 +39,9 @@ void main() {
   if (getMouth(uv) > 0.2 && getMouth(uv) < 0.7) {
     color = getCam(uv);
     // lipgloss
-    //     color*=color;
-    //     color+=0.2;
-    //     color *= vec3(0.98,0.9,1.0);
+    if (luma(color) > 0.6) {
+      color *= 1.1;
+    }
   }
 
   gl_FragColor = vec4(color, 1);
