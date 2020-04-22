d2ae2de57d3d7ec6e52b68f5bdc0f2983ac15309
max bittker <maxbittker@gmail.com>
2020-04-22 02:45:56 -0700

    scale

--- demos/babies.glsl
+++ demos/babies.glsl
@@ -9,7 +9,7 @@ void main() {
     float a = time + TAU * float(i) / 6.;
     vec2 offset = vec2(sin(a), cos(a));
     offset.x /= targetAspect;
-    offset *= length(leftEye - rightEye) * 5.;
+    offset *= length(leftEye - rightEye) * 3.;
     offset += faceCenter;
     vec2 pos = uv * 2.4;
     if (getFace(pos - offset) > 0.1) {
